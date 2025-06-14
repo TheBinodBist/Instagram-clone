ae90825959d0de0dcb2266b0e19b21943c59af63
Binod Bist <thebinodbist@gmail.com>
2025-06-14 09:06:16 +0545

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='text-red-300'>
+      hello
+
+      <Button>hello</Button>
+    </div>
+  )
+}
+
+export default App
